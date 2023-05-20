528ede27413a69ab85d9214c3fc0ece9cf1aabe2
Michael Bestas <mkbestas@lineageos.org>
2023-05-20 22:58:22 +0300

    redbull: Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf
    Signed-off-by: Sukrut4778 <sukrut4778@gmail.com>

--- device-evolution.mk
+++ device-evolution.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Lineage Health
+include hardware/google/pixel/lineage_health/device.mk
+
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-evolution
 
