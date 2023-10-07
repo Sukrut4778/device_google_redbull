54ec64bcd33d7b78689db69a6bc337c35b7f4214
Sukrut4778 <sukrut4778@gmail.com>
2023-10-07 13:43:11 +0530

    Revert "redbull: switch to EROFS"
    This reverts commit 9b09ee7d65dc6cf20466af18930751509d3cae9c.
    
    Signed-off-by: Sukrut4778 <sukrut4778@gmail.com>

--- device-common.mk
+++ device-common.mk
@@ -168,7 +168,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 PRODUCT_PACKAGES += \
@@ -184,7 +184,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
+    FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 # Userdata Checkpointing OTA GC
